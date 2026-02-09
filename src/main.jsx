0bf13c45a0a6570a45a921d8b85da144a123b578
ashutosh24sharma <151420381+ashutosh24sharma@users.noreply.github.com>
2026-02-09 22:12:37 +0530

    hashrouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
-import { StrictMode } from 'react'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { HashRouter } from "react-router-dom";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-     <BrowserRouter>
-    <App />
-    </BrowserRouter>
-  </StrictMode>,
-)
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </StrictMode>
+);
